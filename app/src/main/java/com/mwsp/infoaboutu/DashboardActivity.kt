333384ed6e2f7cec7d5f999a0d76cf9bb747a683
Adjie <hawaiiditamang@gmail.com>
2021-11-13 23:08:50 +0700

    Versi Ketiga : Penambahan Detail Data

--- app/src/main/java/com/mwsp/infoaboutu/DashboardActivity.kt
+++ app/src/main/java/com/mwsp/infoaboutu/DashboardActivity.kt
@@ -6,9 +6,20 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 
-class DashboardActivity : AppCompatActivity() {
+class DashboardActivity : AppCompatActivity(), View.OnClickListener{
+
+    private lateinit var bacaBerita : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        bacaBerita = findViewById(R.id.btn_lihat)
+        bacaBerita.setOnClickListener(this)
+
+    }
+    override fun onClick(p0: View?) {
+        val intent = Intent(this, ReadNewsActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/mwsp/infoaboutu/ReadNewsActivity.kt
@@ -0,0 +1,11 @@
+package com.mwsp.infoaboutu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReadNewsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_read_news)
+    }
+}
